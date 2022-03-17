41daf36dffc24e9fcd87cc3c61d8b8b0c30761dd
Sander Derks <sander1derks@live.nl>
2022-03-17 22:49:01 +0100

    Added player input listener + unregister gui listeners onclose

---
+++ src/main/kotlin/me/sd_master92/core/input/PlayerChatInput.kt
@@ -0,0 +1,83 @@
+package me.sd_master92.core.input
+
+import me.sd_master92.core.plugin.CustomPlugin
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.HandlerList
+import org.bukkit.event.Listener
+import org.bukkit.event.player.AsyncPlayerChatEvent
+import org.bukkit.event.player.PlayerCommandPreprocessEvent
+import org.bukkit.event.player.PlayerQuitEvent
+import org.bukkit.scheduler.BukkitRunnable
+
+abstract class PlayerChatInput(
+    private val plugin: CustomPlugin,
+    private val player: Player,
+    private val allowCancel: Boolean = true
+) : Listener
+{
+    abstract fun onInputReceived(input: String)
+    abstract fun onCancel()
+
+    @EventHandler
+    fun onPlayerChat(event: AsyncPlayerChatEvent)
+    {
+        if (event.player == player)
+        {
+            event.isCancelled = true
+            object : BukkitRunnable()
+            {
+                override fun run()
+                {
+                    processInput(event.message)
+                }
+            }.runTask(plugin)
+        }
+    }
+
+    @EventHandler
+    fun onCommandProcess(event: PlayerCommandPreprocessEvent)
+    {
+        if (event.player == player)
+        {
+            event.isCancelled = true
+            processInput(event.message)
+        }
+    }
+
+    @EventHandler
+    fun onPlayerLeave(event: PlayerQuitEvent)
+    {
+        if (event.player == player)
+        {
+            cancel()
+        }
+    }
+
+    fun cancel()
+    {
+        HandlerList.unregisterAll(this)
+    }
+
+    private fun processInput(input: String)
+    {
+        if (allowCancel && input == "cancel")
+        {
+            cancel()
+            onCancel()
+        } else
+        {
+            onInputReceived(input)
+        }
+    }
+
+    private fun initialize()
+    {
+        plugin.registerListener(this)
+    }
+
+    init
+    {
+        initialize()
+    }
+}
--- src/main/kotlin/me/sd_master92/core/inventory/GUI.kt
+++ src/main/kotlin/me/sd_master92/core/inventory/GUI.kt
@@ -6,6 +6,7 @@ import org.bukkit.ChatColor
 import org.bukkit.Material
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
+import org.bukkit.event.HandlerList
 import org.bukkit.event.Listener
 import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.event.inventory.InventoryCloseEvent
@@ -52,6 +53,7 @@ abstract class GUI @JvmOverloads constructor(
     {
         if (isThisInventory(event) && !cancelCloseEvent)
         {
+            HandlerList.unregisterAll(this)
             onClose(event, event.player as Player)
         }
     }
